848d898d02fb64add5cf2786e8995358d61bea8d
hambirNishant <143251007+hambirNishant@users.noreply.github.com>
2023-12-14 00:09:53 +0530

    done

---
+++ realestate/src/Component/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import'./Header.css';
+const Header = () => {
+  return (
+    <section class="h-wrapper">
+        <div className="h-container flexCenter paddings innerwidth ">
+            <img src="./logo.png" alt="logo" width={100} />
+            <div className="h-menu">
+                <a href="">
+
+                    Residencies
+                </a>
+                <a href="">Our value</a>
+                <a href="">Contact us </a>
+                <a href="">Get started</a>
+                <button className='button'>
+                <a href="">Contact</a>
+                </button>
+                
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Header;

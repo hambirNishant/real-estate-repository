e66713dcd64eaa4578724c3fde896c2ef93697f6
hambirNishant <143251007+hambirNishant@users.noreply.github.com>
2023-12-15 19:06:30 +0530

    done

--- realestate/src/App.js
+++ realestate/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Component/Header/Header';
+import Hero from './Component/Hero/Hero';
+
 
 function App() {
   return (
    <Header/>
+   <Hero/>
   );
 }
 
--- realestate/src/Component/Header/Header.jsx
+++ realestate/src/Component/Header/Header.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import'./Header.css';
-const Header = () => {
-  return (
-    <section class="h-wrapper">
-        <div className="h-container flexCenter paddings innerwidth ">
-            <img src="./logo.png" alt="logo" width={100} />
-            <div className="h-menu">
-                <a href="">
+  import React from 'react'
+  import'./Header.css';
+  const Header = () => {
+    return (
+      <section className="h-wrapper">
+          <div className="h-container flexCenter paddings innerwidth ">
+              <img src="./logo.png" alt="logo" width={100} />
+              <div class="flexCenter h-menu">
+                  <a href="">
 
-                    Residencies
-                </a>
-                <a href="">Our value</a>
-                <a href="">Contact us </a>
-                <a href="">Get started</a>
-                <button className='button'>
-                <a href="">Contact</a>
-                </button>
-                
-            </div>
-        </div>
-    </section>
-  )
-}
+                      Residencies
+                  </a>
+                  <a href="">Our value</a>
+                  <a href="">Contact us </a>
+                  <a href="">Get started</a>
+                  <button className='button'>
+                  <a href="">Contact</a>
+                  </button>
+                  
+              </div>
+          </div>
+      </section>
+    )
+  }
 
-export default Header;
+  export default Header;

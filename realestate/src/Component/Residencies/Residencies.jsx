006f2401b28f08bc2ecacbda23efb3a37c5f8b3c
hambirNishant <143251007+hambirNishant@users.noreply.github.com>
2023-12-20 22:16:42 +0530

    done

--- realestate/src/Component/Residencies/Residencies.jsx
+++ realestate/src/Component/Residencies/Residencies.jsx
@@ -3,6 +3,7 @@ import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
 import"swiper/css";
 import data from '../../Utils/slider.json'
 import'./Residencies.css';
+import { sliderSetting } from '../../Utils/Common';
 const Residencies = () => {
   return (
         <section className="r-wrapper">
@@ -15,6 +16,25 @@ const Residencies = () => {
                         Popular Residencies
                     </span>
                 </div>
+                <Swiper {...sliderSetting}>
+                    {
+                        data.map((card,i)=>(
+                            <SwiperSlide key={i}>
+                                <div className='flexColStart r-card'>
+                                    <img src={card.image} alt='home'/>
+                                    <span className="secondaryText r-price">
+                                        <span style={{color:"orange"}}>$</span>
+                                            <span>
+                                                {card.price}
+                                            </span>
+                                    </span>
+                                    <span className='primaryText'>{card.name}</span>
+                                    <span className='secondaryText'>{card.detail}</span>
+                                </div>
+                            </SwiperSlide>
+                        ))
+                    }
+                </Swiper>
             </div>
         </section>
   )

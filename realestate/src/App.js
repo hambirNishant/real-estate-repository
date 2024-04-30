99c8d5b824bbd67d35853e1468d065489dd6cc59
hambirNishant <143251007+hambirNishant@users.noreply.github.com>
2024-04-30 09:14:29 +0530

    Update App.js

--- realestate/src/App.js
+++ realestate/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Component/Header/Header';
-import Hero from './Component/Hero/hero';
+import Hero from './Component/hero/hero';
 import Companies from './Component/Companies/Companies';
 import Residencies from './Component/Residencies/Residencies';
 import Value from './Component/Value/Value';
